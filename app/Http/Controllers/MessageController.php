3a2ed624569437eb26054513888429d15c4496aa
kdbdevelopment <kevindelbello@gmail.com>
2019-10-06 23:26:20 -0300

    Actualizado con mensajes

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        $messages = Message::where('user_id', Auth::id())->get();
+        $users = User::where('id', '<>',Auth::id())->get();
+        return view('messages', ['Messages'=> $messages,'Users'=> $users]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $message = new Message;
+        $message->text = $request->text;
+        $message->user_id = Auth::id();
+        $message->user_id_to = $request->destination;
+        $message->save();
+
+        return redirect()->route('messages');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::post('/nuevasil', 'LoveController@store')->name('love');
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware(CheckKlass::class);
 Route::post('/love', 'LoveController@store')->name('love');
+Route::post('/message', 'MessageController@store')->name('message');
+Route::get('/messages', 'MessageController@index')->name('messages');
 
 Route::get('/profile/{id}', 'ProfileController@show')->name('profile');
 Route::post('/profile', 'ProfileController@update')->name('profile.update');
